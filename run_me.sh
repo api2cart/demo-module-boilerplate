6e367801b0b7f330733104f1765d0c1369854722
Andrii Bidlovskyi <angryalf@gmail.com>
2020-05-06 11:57:22 +0300

    APITOCART-13891 added filtesr for stores, orders, customers, products, categories

---
+++ run_me.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+dir=$(pwd)
+
+#check if config exists
+CONFIGFILE="$dir/src/.env"
+if [ -f "$CONFIGFILE" ]; then
+    echo "config $CONFIGFILE exist"
+else
+    echo "config do not exists"
+    cp "$dir/src/.env.example" "$dir/src/.env"
+fi
+
+# give rw permissions for temorary folder
+sudo chmod -R 777 $dir/src/storage
+
+#stop for any case container
+if ! [ -x "$(command -v docker-compose)" ]; then
+  echo "Please check if Docker installed and runned."
+
+  if ! [ -x "$(command -v xdg-open)" ]; then
+    open https://docs.docker.com/get-docker/
+    exit 1
+  fi
+
+  xdg-open https://docs.docker.com/get-docker/
+  exit 1
+fi
+
+docker-compose down
+
+
+#run containers in background
+docker-compose up -d
+
+if [ $? -eq 0 ];
+then
+    echo "Looks all ok, update related dependencies..."
+    docker-compose run --rm composer update
+    docker-compose run app php artisan migrate
+    docker-compose run app php artisan db:seed
+else
+     echo "missing start please contact support"
+     exit
+fi
+
+if ! [ -x "$(command -v xdg-open)" ]; then
+    open http://localhost:8080
+    exit 0
+fi
+xdg-open http://localhost:8080
