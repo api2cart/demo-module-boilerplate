9c18eb2c8298ed123522f8adaed8b8618437819a
Andrii Bidlovskyi <angryalf@gmail.com>
2020-05-13 13:04:14 +0300

    APITOCART-13880 added prices change for variants

--- src/.phpstorm.meta.php
+++ src/.phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-05-12 07:06:35.
+    * Generated on 2020-05-12 08:56:57.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
