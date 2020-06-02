a3c3ad8f688e2dc615c846f01e19c3c6848f6586
Andrii Bidlovskyi <angryalf@gmail.com>
2020-06-02 11:32:41 +0300

    APITOCART-13912 changes and fixes

--- src/_ide_helper.php
+++ src/_ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 7.13.0 on 2020-06-01 09:01:40.
+ * Generated for Laravel 7.13.0 on 2020-06-02 08:30:56.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
