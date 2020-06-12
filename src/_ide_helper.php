1532bed93aa71fc7c0a2a60af219d803b413ec52
Andrii Bidlovskyi <angryalf@gmail.com>
2020-06-12 13:56:32 +0300

    test cases

--- src/_ide_helper.php
+++ src/_ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 7.14.1 on 2020-06-03 06:41:43.
+ * Generated for Laravel 7.15.0 on 2020-06-12 07:25:23.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
@@ -18241,7 +18241,7 @@ namespace  {
              * Pass the query to a given callback.
              *
              * @param callable $callback
-             * @return \Illuminate\Database\Query\Builder 
+             * @return \Illuminate\Database\Eloquent\Builder 
              * @static 
              */ 
             public static function tap($callback)
@@ -18556,7 +18556,7 @@ namespace  {
             /**
              * Add a subselect expression to the query.
              *
-             * @param \Closure|\Illuminate\Database\Query\Builder|string $query
+             * @param \Closure|$this|string $query
              * @param string $as
              * @return \Illuminate\Database\Query\Builder 
              * @throws \InvalidArgumentException
