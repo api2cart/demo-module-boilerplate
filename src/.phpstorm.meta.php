cc792c6a3ff3efed0a5d57052baa0ed868862495
Andrii Bidlovskyi <angryalf@gmail.com>
2020-06-12 13:56:32 +0300

    test cases

--- src/.phpstorm.meta.php
+++ src/.phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-06-03 06:41:43.
+    * Generated on 2020-06-12 07:25:23.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
