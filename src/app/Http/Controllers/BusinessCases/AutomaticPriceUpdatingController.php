094041b6b37a1115b8ff5819f8f84eb4dedcbca1
Andrii Bidlovskyi <angryalf@gmail.com>
2020-06-30 19:10:06 +0300

    APITOCART-14038 check SMTP settings APITOCART-14028 generating dummie products in all carts

--- src/app/Http/Controllers/BusinessCases/AutomaticPriceUpdatingController.php
+++ src/app/Http/Controllers/BusinessCases/AutomaticPriceUpdatingController.php
@@ -43,4 +43,56 @@ class AutomaticPriceUpdatingController extends Controller
 
         return redirect(route('business_cases.automatic_price_updating'));
     }
+
+    public function store(Request $request)
+    {
+//        Log::debug( $request->all() );
+        $result = [];
+        $carts = collect($this->api2cart->getCartList());
+
+        $formData = $request->except('_hash');
+        $formData['model'] = $formData['sku'];
+//        $formData['image_url'] = "https://dummyimage.com/600x400/ffffff/0011ff.png&text=Product+Image";
+//        $formData['image_name'] = "0011ff.png&text=Product+Image";
+
+        foreach ($carts as $item){
+
+            $result = $this->api2cart->addProduct($item['store_key'], $formData);
+            Log::debug( print_r($item,1) );
+            Log::debug( print_r($result,1) );
+
+            //TODO: add image for each product
+            if ( isset($result['result']['product_id']) && $result['result']['product_id'] ){
+
+
+                $this->api2cart->addProductImage( $item['store_key'], $result['result']['product_id'],
+                    // image data goes here
+                    [
+                        'type'          => 'base',
+                        'image_name'    => '0011ff.png&text=Product+Image',
+                        'url'           => 'https://dummyimage.com/600x400/ffffff/0011ff.png&text=Product+Image'
+                    ]
+                );
+
+            }
+
+
+            $result[] = [
+                'store_key'     => $item['store_key'],
+                'product_id'    => $result['result']['product_id'],
+                'sku'           => $request->get('sku')
+            ];
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+
 }
