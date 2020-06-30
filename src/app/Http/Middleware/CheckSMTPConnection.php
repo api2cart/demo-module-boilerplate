e50275a0675461e2ac8d74f676822d568e1619d1
Andrii Bidlovskyi <angryalf@gmail.com>
2020-06-30 19:10:06 +0300

    APITOCART-14038 check SMTP settings APITOCART-14028 generating dummie products in all carts

---
+++ src/app/Http/Middleware/CheckSMTPConnection.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Log;
+
+class CheckSMTPConnection
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+//        Log::debug('CheckSMTPConnection');
+
+        $mailMD5 = md5( md5(env('MAIL_HOST')) . md5(env('MAIL_PORT')) . md5(env('MAIL_USERNAME')) . md5(env('MAIL_PASSWORD')) . md5(env('MAIL_ENCRYPTION')) );
+
+        if ( session()->get('mail_md5') === $mailMD5 ) return $next($request);
+
+
+        try{
+
+
+            $this->checkSMTP();
+            session()->put('mail_md5', $mailMD5);
+
+
+        } catch (\Swift_TransportException $e){
+
+//            Log::debug( $e->getMessage() );
+//            Log::debug(' looks mail not setuped correctly ');
+
+            // redirect on screen with mail setting instructions
+            return redirect( '/mail_settings' );
+
+
+        } catch (\Exception $e){
+            Log::debug( $e->getMessage() );
+        }
+
+        return $next($request);
+    }
+
+
+    public function checkSMTP()
+    {
+        $mailer = env('MAIL_DRIVER');
+        $s = Mail::createTransport( config("mail.mailers.{$mailer}") );
+        $s->start();
+
+        Log::debug( 'checked smtp - ok' );
+
+        return 'ok';
+
+
+
+
+    }
+
+
+
+}
--- src/resources/views/business_cases/automatic_price_updating/form.blade.php
+++ src/resources/views/business_cases/automatic_price_updating/form.blade.php
@@ -2,9 +2,9 @@
     $faker = Faker\Factory::create();
 @endphp
 @if(isset($product_id))
-    {!! Form::open(['url' => route('businessCases.automatic_price_updating.update', [$store_id, $product_id]) ]) !!}
+    {!! Form::open(['url' => route('businessCases.automatic_price_updating.update', [$store_id, $product_id]), 'id' => 'form' ]) !!}
 @else
-    {!! Form::open(['url' => route('businessCases.automatic_price_updating.create') ]) !!}
+    {!! Form::open(['url' => route('businessCases.automatic_price_updating.store'),'id' => 'form' ]) !!}
 @endif
 
 <div class="alert alert-danger" role="alert" style="display: none;">
@@ -16,7 +16,7 @@
         <div class="form-group row">
             <label for="name" class="col-4 col-form-label">Name</label>
             <div class="col-8">
-                <input type="text" class="form-control" id="name" name="name" value="{{ (isset($product['name'])) ? $product['name'] : $faker->sentence() }}">
+                <input type="text" required class="form-control" id="name" name="name" value="{{ (isset($product['name'])) ? $product['name'] : $faker->sentence() }}">
                 <div class="invalid-feedback"></div>
             </div>
         </div>
@@ -28,7 +28,7 @@
         <div class="form-group row">
             <label for="name" class="col-4 col-form-label">SKU</label>
             <div class="col-8">
-                <input type="text" class="form-control" id="name" name="name" value="{{ (isset($product['sku'])) ? $product['sku'] : $faker->uuid }}">
+                <input type="text" required class="form-control" id="sku" name="sku" value="{{ (isset($product['sku'])) ? $product['sku'] : $faker->uuid }}">
                 <div class="invalid-feedback"></div>
             </div>
         </div>
@@ -40,7 +40,7 @@
         <div class="form-group row">
             <label for="description" class="col-4 col-form-label">Description</label>
             <div class="col-8">
-                <textarea class="form-control" rows="6" id="description" name="description">{!! (isset($product['description'])) ? $product['description'] : $faker->text() !!}</textarea>
+                <textarea class="form-control" required rows="6" id="description" name="description">{!! (isset($product['description'])) ? $product['description'] : $faker->text() !!}</textarea>
                 <div class="invalid-feedback"></div>
             </div>
         </div>
@@ -52,7 +52,7 @@
         <div class="form-group row">
                 <label for="price" class="col-4 col-form-label">Price</label>
                 <div class="col-8">
-                    <input type="number" class="form-control" id="price" name="price" value="{{ ( isset($product['price']) ) ? $product['price'] : $faker->randomFloat(2,1,100) }}" step="0.01"  >
+                    <input type="number" class="form-control" required id="price" name="price" value="{{ ( isset($product['price']) ) ? $product['price'] : $faker->randomFloat(2,1,100) }}" step="0.01"  >
                     <div class="invalid-feedback"></div>
                 </div>
         </div>
@@ -64,7 +64,7 @@
         <div class="form-group row">
             <label for="price" class="col-4 col-form-label">Quantity</label>
             <div class="col-8">
-                <input type="number" class="form-control" id="quantity" name="quantity" value="{{ ( isset($product['quantity']) ) ? $product['quantity'] : $faker->randomNumber(2) }}" step="1" >
+                <input type="number" class="form-control" required id="quantity" name="quantity" value="{{ ( isset($product['quantity']) ) ? $product['quantity'] : $faker->randomNumber(2) }}" step="1" >
                 <div class="invalid-feedback"></div>
             </div>
         </div>
