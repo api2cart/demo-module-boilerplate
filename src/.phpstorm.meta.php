55e8094cb9eaf9c9196b42d9dcd4b0cb1ee0ef02
Andrii Bidlovskyi <angryalf@gmail.com>
2020-07-02 11:13:38 +0300

    APITOCART-14038 added email config change

--- src/.phpstorm.meta.php
+++ src/.phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-06-25 10:59:20.
+    * Generated on 2020-07-02 06:55:43.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
