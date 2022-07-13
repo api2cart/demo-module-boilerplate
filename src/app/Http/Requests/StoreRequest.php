ba1f5f5f734dd6e7dc631d4d3f8c20460c2f49e9
Taras Kubiv <t.kubiv@magneticone.com>
2022-07-13 12:55:51 +0300

    APITOCART-16685 Store Add fix form

--- src/app/Http/Requests/StoreRequest.php
+++ src/app/Http/Requests/StoreRequest.php
@@ -25,12 +25,53 @@ class StoreRequest extends FormRequest
      */
     public function rules()
     {
-//        Log::debug( print_r( request()->all(),1) );
-
-        return [
+        $rules = [
             'cart_id'   => ['required'],
-            'field.*'   => ['required']
+            'field.*'   => ['required'],
+            'field.store_url' => [
+                'required',
+                'regex:/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/'
+            ]
         ];
+
+        $fields = request()->all();
+
+        if (isset($fields['field']['store_key'])) {
+            $rules['field.store_key'] = [
+                'required',
+                'regex:/^[0-9a-z]{32}$/'
+            ];
+        }
+
+        if (isset($fields['field']['multicred'])) {
+            $filledSet = null;
+
+            foreach ($fields['field']['multicred'] as $key => $field) {
+                foreach ($field as $fieldValue) {
+                    if ($fieldValue !== null) {
+                        $filledSet = $key;
+
+                        break 2;
+                    }
+                }
+            }
+
+            foreach ($fields['field']['multicred'] as $key => $fields) {
+                if ($filledSet !== null && $filledSet !== $key) {
+                    continue;
+                }
+
+                foreach ($fields as $fieldName => $fieldValue) {
+                    $rules['field.multicred.' . $key . '.' . $fieldName] = ['required'];
+
+                    if ($filledSet === null) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        return $rules;
     }
 
     public function messages()
--- src/resources/views/orders/index.blade.php
+++ src/resources/views/orders/index.blade.php
@@ -234,6 +234,7 @@
                 serverSide: false,
                 // ordering: false,
                 data: items,
+                bLengthChange: false,
                 dom: '<"row"<"col"B><"col"l><"col"f>><t><"row"<"col"i><"col">p>',
                 buttons: [
                     {
